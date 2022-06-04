330e4a4beda64230e44f6cf64f4b39e206d74746
VilasGayakwad2021 <100585709+VilasGayakwad2021@users.noreply.github.com>
2022-06-04 13:01:45 +0530

    Add files via upload

---
+++ Programming_Assingment1_Answers.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1. Write a Python program to print &quot;Hello Python&quot;?
+
+
+# In[2]:
+
+
+print('Hello Python')
+
+
+# In[3]:
+
+
+#2. Write a Python program to do arithmetical operations addition and division.?
+
+
+# In[5]:
+
+
+a=int(input('Enter No. 1 ='))
+b=int(input('Enter No. 2 ='))
+c=a+b
+print('Addition of a and b is :', c)
+m=a*b
+print('Muliplication of a and b is :', m)
+
+
+# In[6]:
+
+
+#3. Write a Python program to find the area of a triangle?
+
+
+# In[9]:
+
+
+base=int(input('Enter base triangle:'))
+height=int(input('Enter height of traningle :'))
+Area=1/2*base*height
+print('Area of triangle is ', Area)
+
+
+# In[10]:
+
+
+#Write a Python program to swap two variables?
+
+
+# In[13]:
+
+
+x=int(input('Enter a value :'))
+y=int(input('Enter a value :'))
+print('Value of x is :', x)
+print('Value of y is :', y)
+temp=x
+x=y
+y=temp
+print('Now value of x after swap is :', x)
+print('Now value of y after swap is :', y)
+
+
+# In[14]:
+
+
+#5. Write a Python program to generate a random number?
+
+
+# In[27]:
+
+
+import random as rd
+a=rd.randint(10,90)
+print(a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
